efdae12c06ca0549f75e0a62e56c455ecc0248ab
Marcus Wong Yu Xuan <2603182@sit.singapore.tech.edu.sg>
2026-09-17 21:41:11 +0800

    finish calculatetax and generatereport function. To be reviewed

--- Lab3/modular.auditor.py
+++ Lab3/modular.auditor.py
@@ -3,7 +3,7 @@ def get_valid_input(total,failed):
     while True:
         stock=input("Enter stock quantity: ")
         if stock == "quit":
-            return total
+            return total, failed
         if not stock.isdigit():
             failed+=1
             continue
@@ -13,5 +13,22 @@ def process_delivery(current_total,new_value):
     current_total+=new_value
     return current_total
 
+def calculate_tax(amount):
+    tax=amount/10
+    return tax
 
-print(get_valid_input(total=0,failed=0))
+def generate_report(total_amount,failed_number,tax):
+    print(f"""Total Unit Processed: {total_amount}
+Number of Rejected Entries: {failed_number}
+Total Tax: {tax} """) 
+
+def program():    
+    total_amount,failed_number=get_valid_input(total=0,failed=0)
+    tax=calculate_tax(total_amount)
+    generate_report(total_amount, failed_number, tax)
+
+program()
+
+
+
+
